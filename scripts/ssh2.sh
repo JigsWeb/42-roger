fb3ffd13c7e9c2df113bc9791e374f2108bdb1d8
Anthony M <amoynet@roger.localdomain.com>
2019-02-19 17:55:08 +0100

    ssh config ok

--- scripts/ssh2.sh
+++ scripts/ssh2.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-SSH_PORT="4242"
 SSHD_CONF_FILE_LOC="/etc/ssh/sshd_config"
 
-sudo cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 sudo sed -i -e 's/#PasswordAuthentication yes/PasswordAuthentication no/g;
 	s/#PermitRootLogin prohibit-password/PermitRootLogin no/g;' $SSHD_CONF_FILE_LOC
+sudo service ssh restart
+
+echo "SSH Configuation [OK]"
