b819041ac6720a2b20f2714ffb12bdfaf8ceb18a
Anthony MOYNET <amoynet@e1r4p14.42.fr>
2019-02-19 14:20:43 +0100

    comment install sh

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-sh ./scripts/packages.sh
-echo "Packages installation [OK]"
-sh ./scripts/sudo.sh
-echo "\nAdd $USER to sudo group [OK]"
-sh ./scripts/network.sh
-echo "Network interface configuration [OK]"
-sh ./scripts/ssh.sh
-echo "SSH Configuration [OK]"
-sh ./scripts/iptables.sh
-echo "Iptable configuration [OK]"
+# sh ./scripts/packages.sh
+# echo "Packages installation [OK]"
+# sh ./scripts/sudo.sh
+# echo "\nAdd $USER to sudo group [OK]"
+# sh ./scripts/network.sh
+# echo "Network interface configuration [OK]"
+# sh ./scripts/ssh.sh
+# echo "SSH Configuration [OK]"
+# sh ./scripts/iptables.sh
+# echo "Iptable configuration [OK]"
