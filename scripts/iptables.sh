fac5d5bf3f6b87b1b9ff9cc9406bda16e03fc980
Anthony MOYNET <amoynet@e1r9p8.42.fr>
2019-02-20 20:46:08 +0100

    iptables finish

--- scripts/iptables.sh
+++ scripts/iptables.sh
@@ -12,7 +12,7 @@ sudo iptables -X
 sudo iptables -F
 
 # Block packet to other destionation than port 4242 and 80 (web)
-iptables -mangle -A PREROUTING -p tcp --match multiport ! --dports 22,80 -j DROP
+sudo iptables -mangle -A PREROUTING -p tcp --match multiport ! --dports 22,80 -j DROP
 
 # Block packet from reserved/local IPv4 network
 sudo iptables -t mangle -A PREROUTING -s 127.0.0.1/8 -j DROP
@@ -39,34 +39,34 @@ sudo iptables -t mangle -A PREROUTING -p tcp -m conntrack --ctstate NEW -m tcpms
 
 # Block TCP packets with dumb flags
 
-iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,SYN,RST,PSH,ACK,URG NONE -j DROP 
-iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,SYN FIN,SYN -j DROP 
-iptables -t mangle -A PREROUTING -p tcp --tcp-flags SYN,RST SYN,RST -j DROP 
-iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,RST FIN,RST -j DROP 
-iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,ACK FIN -j DROP 
-iptables -t mangle -A PREROUTING -p tcp --tcp-flags ACK,URG URG -j DROP 
-iptables -t mangle -A PREROUTING -p tcp --tcp-flags ACK,FIN FIN -j DROP 
-iptables -t mangle -A PREROUTING -p tcp --tcp-flags ACK,PSH PSH -j DROP 
-iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL ALL -j DROP 
-iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL NONE -j DROP 
-iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL FIN,PSH,URG -j DROP 
-iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL SYN,FIN,PSH,URG -j DROP 
-iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL SYN,RST,ACK,FIN,URG -j DROP
+sudo iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,SYN,RST,PSH,ACK,URG NONE -j DROP 
+sudo iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,SYN FIN,SYN -j DROP 
+sudo iptables -t mangle -A PREROUTING -p tcp --tcp-flags SYN,RST SYN,RST -j DROP 
+sudo iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,RST FIN,RST -j DROP 
+sudo iptables -t mangle -A PREROUTING -p tcp --tcp-flags FIN,ACK FIN -j DROP 
+sudo iptables -t mangle -A PREROUTING -p tcp --tcp-flags ACK,URG URG -j DROP 
+sudo iptables -t mangle -A PREROUTING -p tcp --tcp-flags ACK,FIN FIN -j DROP 
+sudo iptables -t mangle -A PREROUTING -p tcp --tcp-flags ACK,PSH PSH -j DROP 
+sudo iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL ALL -j DROP 
+sudo iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL NONE -j DROP 
+sudo iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL FIN,PSH,URG -j DROP 
+sudo iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL SYN,FIN,PSH,URG -j DROP 
+sudo iptables -t mangle -A PREROUTING -p tcp --tcp-flags ALL SYN,RST,ACK,FIN,URG -j DROP
 
 # Block ICMP packets
 
-iptables -t mangle -A PREROUTING -p icmp -j DROP
+sudo iptables -t mangle -A PREROUTING -p icmp -j DROP
 
 # Limit TCP connections
 
-iptables -A INPUT -p tcp -m connlimit --connlimit-above 80 -j REJECT --reject-with tcp-reset
+sudo iptables -A INPUT -p tcp -m connlimit --connlimit-above 80 -j REJECT --reject-with tcp-reset
 
 # Limit TCP connections/second
 
-iptables -A INPUT -p tcp -m conntrack --ctstate NEW -m limit --limit 60/s --limit-burst 20 -j ACCEPT 
-iptables -A INPUT -p tcp -m conntrack --ctstate NEW -j DROP
+sudo iptables -A INPUT -p tcp -m conntrack --ctstate NEW -m limit --limit 60/s --limit-burst 20 -j ACCEPT 
+sudo iptables -A INPUT -p tcp -m conntrack --ctstate NEW -j DROP
 
 # Limit TCP reset connection/second
 
-iptables -A INPUT -p tcp --tcp-flags RST RST -m limit --limit 2/s --limit-burst 2 -j ACCEPT 
-iptables -A INPUT -p tcp --tcp-flags RST RST -j DROP
+sudo iptables -A INPUT -p tcp --tcp-flags RST RST -m limit --limit 2/s --limit-burst 2 -j ACCEPT 
+sudo iptables -A INPUT -p tcp --tcp-flags RST RST -j DROP
