63136adf3c7563592df110e7c08ea6f1c0ed0eeb
Anthony MOYNET <amoynet@e2r10p12.42.fr>
2019-02-22 14:27:30 +0100

    crontab

--- scripts/crontab.sh
+++ scripts/crontab.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-COMMAND="sh /etc/updater.sh > /var/log/update_script.log"
+COMMAND="apt-get update && apt-get upgrade"
 
 sudo cp scripts/updater.sh /etc/updater.sh
-sudo echo "0 4 0 0 1 amoynet $COMMAND" >> /etc/crontab
-sudo echo "@reboot amoynet $COMMAND" >> /etc/crontab
+sudo sh -c 'echo "0 4 0 0 1 amoynet $COMMAND" >> /etc/crontab'
+sudo sh -c 'echo "@reboot amoynet $COMMAND" >> /etc/crontab'
