24658d7305da190b7f5302fb290b625e955babd4
m <amoynet@roger.localdomain.com>
2019-02-23 21:10:07 +0100

    update

--- scripts/ssh.sh
+++ scripts/ssh.sh
@@ -6,6 +6,6 @@ SSHD_CONF_FILE_LOC="/etc/ssh/sshd_config"
 sudo apt-get install ssh
 sudo sed -i -e 's/#Port 22/Port '$SSH_PORT'/g;' $SSHD_CONF_FILE_LOC
 sudo service ssh restart
-mkdir ~/.ssh
+mkdir -p ~/.ssh
 
-echo "SSH on port 4242, please upload your id_rsa.pub via scp in ~/.ssh, and run ssh2.sh"
+echo "SSH on port 4242, please upload your id_rsa.pub via scp in ~/.ssh/authorized_keys, and run ssh2.sh"
--- scripts/watcher.sh
+++ scripts/watcher.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-sudo dpkg -l incron
-IS_INCRON_INSTALLED=$(echo $?)
+dpkg -l mail > /dev/null 2>&1 || apt-get -y install mailutils
+cat /etc/crontab | grep watcher > /dev/null 2>&1 || (cp scripts/watcher.sh /usr/local/watcher.sh && echo '0 4 * * * root sh /usr/bin/watcher.sh' >> /etc/crontab)
+cat /etc/crontab.old > /dev/null 2>&1 || cat /etc/crontab > /etc/crontab.old
 
-if [ IS_INCRON_INSTALLED = "1" ]; then
-	sudo apt-get -y install incron
-	reset
+DIFF=$(diff /etc/crontab /etc/crontab.old)
+
+if [ $? = 1 ]; then
+	echo $DIFF | mail -s "Crontab watcher" root
+	cat /etc/crontab > /etc/crontab.old
 fi
-sudo sh -c 'echo "root" >> /etc/incron.allow'
-sudo sh -c "echo '/etc/crontab IN_MODIFY /home/$USER/42-roger/assets/watcher.sh' >> /etc/incron.d/watcher"
-sudo service incron restart
