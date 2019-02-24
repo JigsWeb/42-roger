3b896d40c38bb8f1ed263b424c0fff9ad0bfc151
JigsWeb <anthony.moynet@epitech.eu>
2019-02-24 10:08:51 +0100

    SSL part

--- scripts/web.sh
+++ scripts/web.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-apt-get install nginx nodejs npm
+curl -sL https://deb.nodesource.com/setup_11.x | bash -
+apt-get install -y nginx nodejs
 
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt
+openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
 
+cp assets/self-signed.conf /etc/nginx/snippets/self-signed.conf
+cp assets/ssl-params.conf /etc/nginx/snippets/ssl-params.conf
 
-
+cp /etc/nginx/sites-available/default /etc/nginx/sites-available/default.bak
