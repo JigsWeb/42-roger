c2ff8c444b008a191134a349acf5b10f54f3ce52
amoynet <amoynet@debian.localdomain.com>
2019-02-25 16:46:06 +0100

    watcher update

--- scripts/watcher.sh
+++ scripts/watcher.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 dpkg -l mail > /dev/null 2>&1 || apt-get -y install mailutils
-cat /etc/crontab | grep watcher > /dev/null 2>&1 || (cp scripts/watcher.sh /usr/local/watcher.sh && echo '0 4 * * * root sh /usr/bin/watcher.sh' >> /etc/crontab)
+cat /etc/crontab | grep watcher > /dev/null 2>&1 || (cp scripts/watcher.sh /usr/local/watcher.sh && echo '0 4 * * * root sh /usr/local/watcher.sh' >> /etc/crontab)
 cat /etc/crontab.old > /dev/null 2>&1 || cat /etc/crontab > /etc/crontab.old
 
 DIFF=$(diff /etc/crontab /etc/crontab.old)
