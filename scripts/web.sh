50f2052d7de53e76b1214de0acfdfb087d3e8ded
amoynet <amoynet@debian.localdomain.com>
2019-02-25 18:43:40 +0100

    web update

--- scripts/web.sh
+++ scripts/web.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-curl -sL https://deb.nodesource.com/setup_11.x | bash -
+sudo apt-get install curl software-properties-common
+curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
 apt-get install -y nginx nodejs
 
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt
@@ -10,3 +11,14 @@ cp assets/self-signed.conf /etc/nginx/snippets/self-signed.conf
 cp assets/ssl-params.conf /etc/nginx/snippets/ssl-params.conf
 
 cp /etc/nginx/sites-available/default /etc/nginx/sites-available/default.bak
+cp assets/nginx-default /etc/nginx/sites-available/default
+
+service nginx restart
+
+npm install -g bower nodemon
+
+cp -R assets/html /var/www
+(cd /var/www/html && sudo bower --allow-root install)
+
+cp -R assets/api /opt
+(cd /opt/api && sudo npm install)
