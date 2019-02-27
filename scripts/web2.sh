5c297b76639ac73386a23ff86eb0373469e5588b
Anthony MOYNET <amoynet@e1r11p7.42.fr>
2019-02-27 21:17:44 +0100

    ok

--- scripts/web2.sh
+++ scripts/web2.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo apt-get install apache2-utils
+sudo apt-get install nginx apache2-utils
 
 sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt
 sudo openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
