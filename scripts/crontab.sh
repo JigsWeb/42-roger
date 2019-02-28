07618dfe55144b4f0b100043928817df7997dd1d
Anthony MOYNET <amoynet@e1r11p3.42.fr>
2019-02-28 21:06:36 +0100

    update

--- scripts/crontab.sh
+++ scripts/crontab.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-COMMAND="apt-get -y update && apt-get -y upgrade > /var/log/update_script.log"
+COMMAND="apt-get -y update > /var/log/update_script.log && apt-get -y upgrade >> /var/log/update_script.log"
 
 sudo sh -c "echo '0 4 * * 1 root $COMMAND' >> /etc/crontab"
 sudo sh -c "echo '@reboot root $COMMAND' >> /etc/crontab"
